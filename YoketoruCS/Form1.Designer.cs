7f01ac8579ed48c20eccd0eca53818bc0c85afba
sakamoto-dat122 <167387815+sakamoto-dat122@users.noreply.github.com>
2024-06-21 16:05:27 +0900

    ゲームの更新

--- YoketoruCS/Form1.Designer.cs
+++ YoketoruCS/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             components = new System.ComponentModel.Container();
             timer1 = new System.Windows.Forms.Timer(components);
+            labelTitle = new Label();
+            buttonStart = new Button();
+            labelGameover = new Label();
+            buttonToTitle = new Button();
             SuspendLayout();
             // 
             // timer1
@@ -38,18 +42,71 @@
             timer1.Interval = 50;
             timer1.Tick += timer1_Tick;
             // 
+            // labelTitle
+            // 
+            labelTitle.AutoSize = true;
+            labelTitle.Font = new Font("UD デジタル 教科書体 N-B", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            labelTitle.Location = new Point(340, 123);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(264, 55);
+            labelTitle.TabIndex = 0;
+            labelTitle.Text = "よけとるCS";
+            // 
+            // buttonStart
+            // 
+            buttonStart.BackColor = SystemColors.Menu;
+            buttonStart.Font = new Font("Yu Gothic UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonStart.Location = new Point(315, 271);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(314, 103);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "Start!!";
+            buttonStart.UseVisualStyleBackColor = false;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // labelGameover
+            // 
+            labelGameover.AutoSize = true;
+            labelGameover.Font = new Font("メイリオ", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            labelGameover.ForeColor = Color.Maroon;
+            labelGameover.Location = new Point(313, 139);
+            labelGameover.Name = "labelGameover";
+            labelGameover.Size = new Size(318, 72);
+            labelGameover.TabIndex = 2;
+            labelGameover.Text = "GAMEOVER";
+            // 
+            // buttonToTitle
+            // 
+            buttonToTitle.BackColor = SystemColors.Control;
+            buttonToTitle.Font = new Font("メイリオ", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonToTitle.Location = new Point(315, 271);
+            buttonToTitle.Name = "buttonToTitle";
+            buttonToTitle.Size = new Size(314, 103);
+            buttonToTitle.TabIndex = 3;
+            buttonToTitle.Text = "タイトルへ";
+            buttonToTitle.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(944, 501);
+            Controls.Add(buttonToTitle);
+            Controls.Add(labelGameover);
+            Controls.Add(buttonStart);
+            Controls.Add(labelTitle);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private Label labelTitle;
+        private Button buttonStart;
+        private Label labelGameover;
+        private Button buttonToTitle;
     }
 }
--- YoketoruCS/Form1.cs
+++ YoketoruCS/Form1.cs
@@ -10,11 +10,11 @@ namespace YoketoruCS
             Gameover,
             Clear
         }
-        
+
         /// <summary>
         /// 次に切り替えたい状態
         /// </summary>
-        State nextStage = State.Title;
+        State nextState = State.Title;
 
         /// <summary>
         /// 現在の状態
@@ -28,7 +28,53 @@ namespace YoketoruCS
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
+            InitState();
+            UpdateState();
+        }
+
+        void InitState()
+        {
+            if (nextState == State.None)
+            {
+                return; //実行されるとメソッドを終了して呼び出し元に処理が戻る
+            }
+            currentState = nextState;
+            nextState = State.None;
+
+
+            //初期化処理
+            switch (currentState)
+            {
+                case State.Title:
+                    labelGameover.Visible = false;
+                    buttonToTitle.Visible = false;
+                    break;
+
+                case State.Game:
+                    labelTitle.Visible = false;
+                    buttonStart.Visible = false;
+                    break;
+            }
+        }
+
+        void UpdateState()
+        {
+            switch(currentState)
+            {
+                case State.Game:
+                    UpdateGame();
+                    break;
+            }
+        }
+
+        void UpdateGame()
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            nextState = State.Game;
         }
     }
 }
