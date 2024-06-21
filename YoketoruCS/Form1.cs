34cf1c47597ac0a75d81b7dc690664401b7ecf50
sakamoto-dat122 <167387815+sakamoto-dat122@users.noreply.github.com>
2024-06-21 15:10:53 +0900

    状態定義

--- YoketoruCS/Form1.cs
+++ YoketoruCS/Form1.cs
@@ -2,9 +2,33 @@ namespace YoketoruCS
 {
     public partial class Form1 : Form
     {
+        enum State
+        {
+            None = -1,
+            Title,
+            Game,
+            Gameover,
+            Clear
+        }
+        
+        /// <summary>
+        /// Ÿ‚ÉØ‚è‘Ö‚¦‚½‚¢ó‘Ô
+        /// </summary>
+        State nextStage = State.Title;
+
+        /// <summary>
+        /// Œ»İ‚Ìó‘Ô
+        /// </summary>
+        State nextState = State.None;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
